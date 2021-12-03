8d22d97106ea181775363c57047b074c4533f5f8
John31337 <john3133747@outlook.com>
2021-12-03 20:13:23 +0800

    create

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class GitHubSearch extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    constructor(props){
+        super(props);
+        this.textInput = null;
+
+        this.setTextInputRef = element => {
+            this.textInput = element;
+        }
+
+        this.state = {
+            username: ''
+        };
+    }
+
+    getUser() {
+        alert(this.textInput.value);
+        return fetch('https://api.github.com/users/' + this.textInput.value)
+            .then(response => response.json())
+            .then(response => {
+                return response;
+            })
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        let user = await this.getUser();
+        this.setState({
+            username: user.login,
+            id: user.id,
+            url: user.url,
+            avatar_url: user.avatar_url,
+        });
+    }
+
+    render() {
+        let user;
+        if(this.state.username) {
+            user =
+                <div>
+                    <p>{this.state.username}
+                        <br/>
+                        {this.state.id}
+                        <br/>
+                    </p>
+                    <img src={this.state.avatar_url}/>
+                </div>
+        }
+
+        return (
+            <div>
+                <header>
+                    <h1>Github User Search </h1>
+                </header>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <input ref={this.setTextInputRef} type='text' placeholder='username' />
+                    <input type="submit" placeholder="Send"/>
+                </form>
+                <p>
+                    {user}
+                </p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<GitHubSearch/>, document.getElementById('root'));
